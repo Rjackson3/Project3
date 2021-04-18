3f281a1f4d1a49617c29985a6cfc4725b79efa54
Andy Qiao <77704448+aqiao-814@users.noreply.github.com>
2021-04-18 16:37:26 -0500

    added box slider

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.slider').bxSlider();
+});
+
+$(function(){
+  $('.bxslider').bxSlider({
+    mode: 'fade',
+    captions: true,
+    slideWidth: 600
+  });
+});

a33c85f983ecb93587c7066e42c6817ab2de874c
Andy Qiao <77704448+aqiao-814@users.noreply.github.com>
2021-04-19 19:43:33 -0500

    added the google maps requirement

--- js/script.js
+++ js/script.js
@@ -15,3 +15,38 @@ $(document).ready(function(){
       $('#aimg').toggleClass('aboutmeimg2');
   });
 });
+
+function initMap() {
+  const chicago = { lat: 41.881832, lng: -87.623177 };
+  const school = { lat: 41.87311896959141, lng: -87.62791057334427};
+  const neighborhood = { lat: 41.97981509067583, lng: -87.69937281857815};
+  const IIT = { lat: 41.83429528028157, lng: -87.62756505033413 }
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 11,
+    center: chicago,
+  });
+  
+  const marker = new google.maps.Marker({
+    position: IIT,
+    map: map,
+  });
+  
+  const image =
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+  const beachMarker = new google.maps.Marker({
+    position: neighborhood,
+    map,
+    icon: image,
+  });
+  
+  new google.maps.Marker({
+    position: school,
+    icon: {
+      path: google.maps.SymbolPath.CIRCLE,
+      scale: 7,
+    },
+    draggable: false,
+    map: map,
+  });
+}
+
